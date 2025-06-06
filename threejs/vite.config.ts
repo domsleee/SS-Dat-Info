287931b391d60b03eb2b8d8c276dec7f201cc4d3
Dom Slee <domslee1@gmail.com>
2025-06-06 22:43:13 +1000

    Small fixes

--- threejs/vite.config.ts
+++ threejs/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import { readdir } from 'fs/promises';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ command }) => ({
   base: command === 'serve' ? '/' : '/SS-Dat-Info/',
   appType: 'mpa',
   server: {
-    port: '8080',
+    port: 8080,
     fs: {
       allow: ['..', '../replays']
     },
@@ -18,21 +18,21 @@ export default defineConfig(({ command }) => ({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),  // eslint-disable-line no-undef
-        circleTool: path.resolve(__dirname, 'circleTool/index.html')  // eslint-disable-line no-undef
+        main: path.resolve(__dirname, 'index.html'),
+        circleTool: path.resolve(__dirname, 'circleTool/index.html')
       }
     }
   },
   publicDir: {
     '/replays': '../replays'
-  },
+  } as unknown as string,
   plugins: [
     tailwindcss(),
     {
       name: 'list-public-dir',
       configureServer(server) {
         server.middlewares.use('/api/files', async (req, res) => {
-          const dir = path.resolve(__dirname, 'public');  // eslint-disable-line no-undef
+          const dir = path.resolve(__dirname, 'public');
           const files = await readdir(dir, { recursive: true });
           res.setHeader('Content-Type', 'application/json');
           res.end(JSON.stringify(files));
@@ -41,7 +41,7 @@ export default defineConfig(({ command }) => ({
     },
   ],
   define: {
-    APP_VERSION: JSON.stringify(process.env.npm_package_version),  // eslint-disable-line no-undef
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
   },
   resolve: {
     alias: {
