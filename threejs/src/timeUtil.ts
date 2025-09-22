48aaffd6fdeac28364cec7920ccdc646ae153ce9
Dominic Slee <domslee1@gmail.com>
2025-09-22 19:50:01 +1000

    Use interpolation with collision data to increase precision more than 0.01s (#39)

--- threejs/src/timeUtil.ts
+++ threejs/src/timeUtil.ts
@@ -13,3 +13,17 @@ export function getHumanReadableTime(analyzeResult: AnalyzeResult, frame: number
 export function msToHumanReadable(ms: number) {
   return new Date(ms).toISOString().slice(14, 22).replace(".", ":");
 }
+
+export function msToHumanReadableWithMs(ms: number) {
+  const DP = 4;
+  const MILLISECOND_DIGITS = 3;
+  const pow10 = Math.pow(10, DP - MILLISECOND_DIGITS);
+  const intMs = Math.trunc(ms % 1000);
+  let nDigits = intMs * pow10;
+  if (DP > MILLISECOND_DIGITS) {
+    nDigits += Math.round((ms % 1) * pow10);
+  }
+
+  return new Date(ms).toISOString().slice(14, 20)
+    + String(nDigits).padStart(DP, '0');
+}
